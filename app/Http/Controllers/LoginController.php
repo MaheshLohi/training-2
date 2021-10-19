c8333722788c1b465d61c9ee1d612935acc4f592
Sam Sunil <sam.sunil@OPN-MC-092.local>
2021-10-19 12:42:25 +0530

    mod

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,45 @@
+<?php 
+namespace App\Http\Controllers;
+
+use App\services\LoginServices;
+use Exception;
+// use Illuminate\Http\Request;
+
+class LoginController extends Controller {
+    public $apiCall;
+    public function __construct(LoginServices $apiCall) {
+        $this->apiCall = $apiCall;
+    }
+
+    public function login(){
+        $user = $this->apiCall->loginApi();
+        try {
+            session(['user' => $user->data]);
+            return redirect(('/profile'));
+        } catch(Exception $e) {
+            return view('error',['error'=>'Unable to login please try after sometime']);
+        }
+    }
+
+    public function logout()
+    {
+        $user = session('user');
+        //print_r($user->email);
+       try{
+        $this->apiCall->logoutApi($user->email, $user->token);
+        return redirect('/login')->with('alert', 'Logged out successfully');
+       }catch(Exception $e){
+           return view('error',['error'=>'An error occured']);
+       }
+    }
+    public function userHome()
+    {
+        if (!session('user')) {
+            return redirect('/login');
+        } else {
+            $userData = session('user');
+            return view('user_home', ['user' => $userData]);
+        }
+    }
+}
+?>
---
+++ app/services/LoginServices.php
@@ -0,0 +1,37 @@
+<?php 
+namespace App\services;
+
+class LoginServices {
+    public function loginApi(){
+        $ch = curl_init();
+        $url = 'https://npci-qa-api.open.money/api/consumer/login';
+
+        $data = array("email" => "anandhuks0@gmail.com", "password" => array(
+            "ct" => "zyeib3uuUnTqR3Zf1oL3vg==",     "iv" => "42925de56071ba11bb984cc8bfbcf350",    "s" => "3b914d6469e30320"
+        ), "user_type" => "2", "login_type" => "0");
+
+
+        $postdata = json_encode($data);
+        
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch,CURLOPT_POSTFIELDS,$postdata);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        $result = curl_exec($ch);
+        return json_decode($result);
+
+    }
+
+    public function logoutApi($result, $token) {
+        $ch = curl_init();
+        $url = "https://npci-qa-api.open.money/api/consumer/logout?email=$email&Authorization=$token";
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response = json_decode(curl_exec($ch), true);
+        return $response['message'];
+    }
+
+}
+
+?>
